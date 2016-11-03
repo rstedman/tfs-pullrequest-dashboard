376b9c9b58aa7827951713d61b7c114d61784128
Ryan Stedman <ryan.stedman@gmail.com>
2016-11-03 15:11:16 -0400

    fix storage service provider

--- src/app/storageService.provider.ts
+++ src/app/storageService.provider.ts
@@ -5,21 +5,20 @@ import { LocalStorageService } from "./localStorage.service";
 import { TfsStorageService } from "./tfsStorage.service";
 import { AppConfigService } from "./appConfig.service";
 
-/** factory provider for the tfsservice, which switches the backend provider based on if it's using tfs online,
-    an on-prem service **/
+/**
+    factory provider for the tfsservice, which switches the backend provider based on if it's using tfs online,
+    an on-prem service
+**/
 @Injectable()
 export class StorageServiceProvider implements FactoryProvider {
   public provide = StorageService;
-
-  // expected to be set before the app is bootstrapped, if running as an extension
-  public static dataService: IExtensionDataService;
 
   public useFactory(zone: NgZone, config: AppConfigService): StorageService {
     // If we aren't running as a VSS extension, use the LocalStorageService
     if(config.devMode) {
       return new LocalStorageService();
     } else {
-      return new TfsStorageService(StorageServiceProvider.dataService, zone);
+      return new TfsStorageService(config.extensionDataService, zone);
     }
   }
 
