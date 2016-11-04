4994b6d9acd9a191b7b6c0011dbfda74470fb439
Ryan Stedman <ryan.stedman@gmail.com>
2016-11-04 19:50:36 -0400

    refactor bundling process to allow quicker builds while debugging

--- gulpfile.js
+++ gulpfile.js
@@ -9,35 +9,36 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var run = require('gulp-run');
 var embedTemplates = require('gulp-angular2-embed-templates');
+var path = require('path');
+var htmlreplace = require('gulp-html-replace');
 
 var paths = {
     buildFiles: ['./gulpfile.js', './package.json', './typings.json', './tsconfig.json', './system.config.js'],
-    static: ['src/**/*', '!**/*.ts', '!**/*.js', '!src/app/**/*.html'],
     compiledFiles: ['src/**/*.ts', 'typings/index.d.ts', 'src/**/*.js'],
     vendor_js: ['node_modules/zone.js/dist/zone.js','node_modules/reflect-metadata/Reflect.js', 'node_modules/systemjs/dist/system.src.js', 'node_modules/es6-shim/es6-shim.js', 'node_modules/vss-web-extension-sdk/lib/VSS.SDK.js'],
-    buildOut: 'build/target'
+    buildOut: 'build/'
 }
 
 gulp.task('clean', function() {
     return del(['build']);
 });
 
-gulp.task('copy:static', function() {
-    return gulp
-        .src(paths.static)
-        .pipe(gulp.dest(paths.buildOut));
-});
+
+gulp.task('copy:vendor', function() {
+    return gulp.src(['node_modules/@angular/**/*', 'node_modules/rxjs/**/*', 'node_modules/typescript/**/*', 'node_modules/plugin-typescript/**/*'], {base: './node_modules'})
+        .pipe(gulp.dest('./build/vendor'));
+})
 
 gulp.task('copy:multiselect-src', function() {
     // the multiselect-src module isn't packaged with compiled sources, so instead copy it into the app so it can be
     // compiled with it.
     return gulp.src(['./node_modules/angular-2-dropdown-multiselect/src/multiselect-dropdown.ts'])
-        .pipe(gulp.dest('./build/compile/app/'));
+        .pipe(gulp.dest('./build/app/'));
 });
 
-gulp.task('bundle:app', ['copy:multiselect-src'], function() {
-    var builder = new systemjsBuilder('.', './system.config.js');
-    return builder.buildStatic('./build/compile/app/app.ts', paths.buildOut + '/app/app.bundle.min.js',
+gulp.task('bundle:app', function() {
+    var builder = new systemjsBuilder('build/', 'build/system.config.js');
+    return builder.buildStatic('./build/app/app.ts', './build/app/app.bundle.min.js',
         {
             sourceMaps: true,
             runtime: false,
@@ -46,6 +47,17 @@ gulp.task('bundle:app', ['copy:multiselect-src'], function() {
             //minify: true
         });
 });
+
+gulp.task('html:replace', function() {
+    return gulp.src('build/index.html')
+            .pipe(htmlreplace({
+                'load_bundle': 'app/app.bundle.min.js'
+            },
+            {
+                keepUnassigned: false //will remove any build sections that we don't have any explicit actions for
+            }))
+            .pipe(gulp.dest('build/'));
+})
 
 gulp.task('bundle:vendor', function() {
     return gulp.src(paths.vendor_js)
@@ -57,44 +69,42 @@ gulp.task('bundle:vendor', function() {
         .pipe(gulp.dest(paths.buildOut + '/vendor'));
 });
 
-gulp.task('compile:copy', function() {
-    return gulp.src('./src/**/*')
-        .pipe(gulp.dest('./build/compile'));
+gulp.task('compile:copy', ['copy:multiselect-src'], function() {
+    return gulp.src(['./src/**/*', 'tsconfig.json'])
+        .pipe(gulp.dest('./build'));
 });
 
 gulp.task('compile:embed', function () {
-    return gulp.src('./build/compile/**/*.ts')
+    return gulp.src('./build/app/*.ts')
                .pipe(embedTemplates({sourceType:'ts'}))
-               .pipe(gulp.dest('./build/compile'));
+               .pipe(gulp.dest('./build/app'));
 });
 
-gulp.task('compile', function(callback) {
+// compiles just local sources
+gulp.task('compile:sources', function(callback) {
     runSequence('compile:copy', 'compile:embed', callback);
 });
 
+gulp.task('compile', ['copy:vendor', 'compile:sources']);
 
 gulp.task('build', function(callback) {
-    runSequence('clean', 'compile', ['copy:static', 'bundle:vendor', 'bundle:app'], callback);
+    runSequence('clean', 'compile', ['bundle:vendor', 'bundle:app', 'html:replace'], callback);
 });
 
-
 gulp.task('package', ['build'], function() {
-    return run('tfx extension create --root build\\target --manifest-globs manifest.json  --output-path build\\dist').exec()
+    return run('tfx extension create --root build --manifest-globs manifest.json  --output-path dist').exec()
 });
 
 gulp.task('package:dev', ['build'], function() {
-    return run('tfx extension create --root build\\target --manifest-globs manifest-dev.json  --output-path build\\dist').exec()
+    return run('tfx extension create --root build --manifest-globs manifest-dev.json  --output-path dist').exec()
 });
 
-gulp.task('watch', function() {
-    gulp.watch(paths.buildFiles, ['build']);
-    gulp.watch(paths.static, ['copy:static']);
-    gulp.watch(paths.compiledFiles, ['bundle:app']);
-});
+gulp.task('serve', ['compile'], function() {
 
-gulp.task('serve', function() {
+    gulp.watch(['./src/**/*.*', './tsconfig.json'], ['compile:sources']);
+
     connect.server({
-        root: 'build/target'
+        root: './build'
     });
 });
 
---
+++ src/system.config.js
@@ -0,0 +1,63 @@
+(function(global) {
+
+  var paths = {
+    'npm:': 'vendor/'
+  };
+
+  // map tells the System loader where to look for things
+  var map = {
+    'app': 'app',
+    // angular bundles
+    '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
+    '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
+    '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
+    '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
+    '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+    '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
+    '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
+    '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+    '@angular/upgrade': 'npm:@angular/upgrade/bundles/upgrade.umd.js',
+    'rxjs': 'npm:rxjs',
+    'typescript': 'npm:typescript',
+    'ts': 'npm:plugin-typescript/lib'
+  };
+  // packages tells the System loader how to load when no filename and/or no extension
+  var packages = {
+    'app': { main: 'app.ts',  defaultExtension: 'ts' },
+    'rxjs': { defaultExtension: 'js' },
+    'ts': { 'main': 'plugin.js'},
+    "typescript": {
+      "main": "lib/typescript.js",
+      "meta": {
+        "lib/typescript.js": {
+          "exports": "ts"
+        }
+      }
+    }
+  };
+
+  var config = {
+    transpiler: 'ts',
+    typescriptOptions: {
+      //typeCheck: 'strict',
+      tsconfig: true,
+      typings: {
+        "@angular/common": "index.d.ts",
+        "@angular/compiler": "index.d.ts",
+        "@angular/core": "index.d.ts",
+        "@angular/http": "index.d.ts",
+        "@angular/forms": "index.d.ts",
+        "@angular/platform-browser": "index.d.ts",
+        "@angular/platform-browser-dynamic": "index.d.ts",
+        "rxjs": "Rx.d.ts"
+      },
+    },
+    paths: paths,
+    map: map,
+    packages: packages,
+    meta: {
+      'TFS/*': {build:false}
+    }
+  };
+  System.config(config);
+})(this);
