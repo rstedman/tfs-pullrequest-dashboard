4f09eeb4665daabba1f7d33fe5440c45af30bb3d
Ryan Stedman <ryan.stedman@gmail.com>
2016-11-06 01:42:57 -0400

    finish tests for pullRequestViewModel

--- test/pullRequestViewModel.spec.ts
+++ test/pullRequestViewModel.spec.ts
@@ -91,7 +91,7 @@ describe("PullRequestViewModel", () => {
             createdBy: {
                 displayName: details.createdById,
                 id: details.createdById,
-                url: null,
+                url: `http://images/${details.createdById}`,
                 imageUrl: null,
                 isAadIdentity: false,
                 isContainer: false,
@@ -119,8 +119,8 @@ describe("PullRequestViewModel", () => {
         return result;
     }
 
-    it("Initializes Simple Properties", () => {
-        let pr = createPullRequest({
+    function getSimplePR(): GitPullRequest {
+        return createPullRequest({
             created: new Date(),
             createdById: "user1",
             id: 1,
@@ -136,7 +136,10 @@ describe("PullRequestViewModel", () => {
                 }
             ]
         });
+    }
 
+    it("Initializes Simple Properties", () => {
+        let pr = getSimplePR();
         let subject = new PullRequestViewModel(pr, defaultRepository, defaultUser);
         expect(subject.reviewers.length).toEqual(pr.reviewers.length);
         expect(subject.createdBy).toEqual(pr.createdBy.displayName);
@@ -144,5 +147,99 @@ describe("PullRequestViewModel", () => {
         expect(subject.repositoryName).toEqual(defaultRepository.name);
         expect(subject.sourceRefName).toEqual(pr.sourceRefName);
         expect(subject.targetRefName).toEqual(pr.targetRefName);
+        expect(subject.createdByImageUrl).toEqual(pr.createdBy.imageUrl);
+        expect(subject.hasMergeConflicts).toEqual(false);
+    });
+
+    it("Removes refs/heads from refNames", () => {
+        let pr = getSimplePR();
+        pr.sourceRefName = "refs/heads/my_branch";
+        pr.targetRefName = "refs/heads/master";
+        let subject = new PullRequestViewModel(pr, defaultRepository, defaultUser);
+        expect(subject.sourceRefName).toEqual("my_branch");
+        expect(subject.targetRefName).toEqual("master");
+    });
+
+    it("sets reviewers to empty array if reviewers null", () => {
+        let pr = getSimplePR();
+        pr.reviewers = null;
+        let subject = new PullRequestViewModel(pr, defaultRepository, defaultUser);
+        expect(subject.reviewers).toBeDefined();
+        expect(subject.reviewers.length).toEqual(0);
+    });
+
+    it("sets requestedByMe correctly", () => {
+        let pr = getSimplePR();
+        pr.createdBy.id = defaultUser.id;
+        let subject = new PullRequestViewModel(pr, defaultRepository, defaultUser);
+        expect(subject.requestedByMe).toEqual(true);
+        expect(subject.assignedToMe).toEqual(false);
+        expect(subject.assignedToMyTeam).toEqual(false);
+    });
+
+    it("sets assignedToMe correctly", () => {
+        let pr = getSimplePR();
+        pr.reviewers.push(
+            voterToIdentityRef({
+                id: defaultUser.id,
+                required: false,
+                vote: 0
+            }));
+        let subject = new PullRequestViewModel(pr, defaultRepository, defaultUser);
+        expect(subject.requestedByMe).toEqual(false);
+        expect(subject.assignedToMe).toEqual(true);
+        expect(subject.assignedToMyTeam).toEqual(false);
+    });
+
+    it("sets assignedToMyTeam correctly", () => {
+        let pr = getSimplePR();
+        pr.reviewers.push(
+            voterToIdentityRef({
+                id: "group2",
+                required: false,
+                vote: 0
+            }));
+        let subject = new PullRequestViewModel(pr, defaultRepository, defaultUser);
+        expect(subject.requestedByMe).toEqual(false);
+        expect(subject.assignedToMe).toEqual(false);
+        expect(subject.assignedToMyTeam).toEqual(true);
+    });
+
+    it("sets hasMergeConflicts correctly", () => {
+        let pr = getSimplePR();
+        pr.mergeStatus = PullRequestAsyncStatus.Conflicts;
+        let subject = new PullRequestViewModel(pr, defaultRepository, defaultUser);
+        expect(subject.hasMergeConflicts).toEqual(true);
+    });
+
+    it("sorts required reviewers first", () => {
+        let pr = getSimplePR();
+        pr.reviewers = [
+            voterToIdentityRef({
+                id: "non_req1",
+                required: false,
+                vote: 0
+            }),
+            voterToIdentityRef({
+                id: "req1",
+                required: true,
+                vote: 0
+            }),
+            voterToIdentityRef({
+                id: "non_req2",
+                required: false,
+                vote: 0
+            }),
+            voterToIdentityRef({
+                id: "req2",
+                required: true,
+                vote: 0
+            })];
+        let subject = new PullRequestViewModel(pr, defaultRepository, defaultUser);
+        expect(subject.reviewers.length).toEqual(4);
+        expect(subject.reviewers[0].id).toEqual("req1");
+        expect(subject.reviewers[1].id).toEqual("req2");
+        expect(subject.reviewers[2].id).toEqual("non_req1");
+        expect(subject.reviewers[3].id).toEqual("non_req2");
     });
 });
